4a5f759314d89904cd5d66783f843ad77c0f71a7
jesus.eduardo369 <jesus.eduardo@2amigos.us>
2025-03-23 14:56:39 -0300

    Setting nest app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ApiKeyMiddleware } from './common/middleware/api-key.middleware';
+import { config } from 'dotenv';
+config({ path: `${process.cwd()}/.env` });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.enableCors({
+    origin: 'http://localhost:4000',
+    credentials: true,
+  });
+  const port = process.env.API_PORT || 3001;
+  app.use(new ApiKeyMiddleware().use);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
